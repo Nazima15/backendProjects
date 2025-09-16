3271adb833ea54b856795d7977d20d11f5072395
ergeshovanazima2 <ergeshovanazima2@gmail.com>
2025-09-16 12:33:21 +0900

    Add BGP500 project

---
+++ BGP500/src/main/java/bgp500/LoginController.java
@@ -0,0 +1,53 @@
+package bgp500;
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/loginController")
+public class LoginController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private LoginDAO loginDAO; 
+    private LoginDTO loginDTO;
+
+    public LoginController() {
+        super();
+    }
+
+    public void init(ServletConfig config) throws ServletException {
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        doProcess(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+    throws ServletException, IOException {
+        doProcess(request, response);
+    }
+
+    protected void doProcess(HttpServletRequest request, HttpServletResponse response) 
+    throws ServletException, IOException {
+        String userid = request.getParameter("userid");
+        String passwd = request.getParameter("passwd");
+
+        loginDAO = new LoginDAO();
+        loginDTO = new LoginDTO();
+        loginDTO.setUserID(userid);
+        loginDTO.setUserPW(passwd);
+
+        HttpSession session = request.getSession(true);
+        if("LOGIN".equals(request.getParameter("actionType"))){
+            loginDTO.setLoginState("login");
+            loginDTO = loginDAO.loginAuth(loginDTO);
+
+            session.setAttribute("loginState", loginDTO.getLoginState());
+            session.setAttribute("userID", loginDTO.getUserID());
+            session.setAttribute("userPW", loginDTO.getUserPW());
+        } else {
+            session.invalidate();
+        }
+        response.sendRedirect(request.getContextPath() + "/login.jsp");
+    }
+}
---
+++ BGP500/src/main/webapp/index.jsp
@@ -0,0 +1,38 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+   <h1>BGP Practice #500</h1>
+   <h3>콘트롤로 서블릿, DAO, DTO  클래스 활용</h3>
+   <hr>
+   <a href="<%=request.getContextPath()%>/login.jsp">로그인페이지 (스크립틀릿+HTML)</a><p>
+   <hr>
+   <h3>세션 정보</h3><p>
+   <h3>주의:실무에서 세션에 비밀번호를 저장하지 않음!!</h3><p>
+   <%
+      java.util.Enumeration<String> names=session.getAttributeNames();
+      while(names.hasMoreElements()){
+         String name=names.nextElement();
+         Object value=session.getAttribute(name);
+         out.println(name+" : "+value+"<br>");
+      }
+   %>
+   <hr>
+   <form method="post">
+       <input type="submit" name="removeSessionBtn" value="세션제거">
+   </form>
+   <%
+       if (request.getParameter("removeSessionBtn") != null) {
+           session.invalidate();
+           response.sendRedirect(request.getContextPath() + "/index.jsp");
+           return;
+    }
+   %>
+
+</body>
+</html>
